496928202858b0da1d4cfe1cc6292a5f5f08c096
gaos <hellogaos@gmail.com>
2023-11-29 16:44:07 +0700

    baru bikin layout input usernam, pw dan tombol

--- app/src/main/java/com/example/ngebacot/Login.kt
+++ app/src/main/java/com/example/ngebacot/Login.kt
@@ -9,12 +9,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,9 +51,11 @@ class Login : ComponentActivity() {
 
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseCard() {
     val abu = Color(0xFFa1a1a1)
+    var text by remember { mutableStateOf("") }
     Column (
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
@@ -58,8 +67,6 @@ fun BaseCard() {
             modifier = Modifier
                 .size(width = 240.dp, height = 260.dp)
                 .align(alignment = Alignment.CenterHorizontally)
-
-
         ) {
             Text(
                 text = "Welcome Back",
@@ -67,8 +74,37 @@ fun BaseCard() {
                     .padding(16.dp)
                     .align(alignment = Alignment.CenterHorizontally)
             )
+            Text(
+                text = "Ngebacot",
+                modifier = Modifier
+//                    .padding(6.dp)
+                    .align(alignment = Alignment.CenterHorizontally)
+            )
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Username") }
+            )
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Password") }
+            )
+            Button(onClick = { onClick() }) {
+                Text("Login")
+            }
+            Text(
+                text = "Ngebacot",
+                modifier = Modifier
+                    .padding(6.dp)
+                    .align(alignment = Alignment.CenterHorizontally)
+            )
         }
     }
+
+}
+
+fun onClick() {
 
 }
 
