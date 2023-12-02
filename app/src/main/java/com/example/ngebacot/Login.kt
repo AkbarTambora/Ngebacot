504dce382d17ccf9075dbdcc76304758559a921c
AkbarTambora <77846899+AkbarTambora@users.noreply.github.com>
2023-12-02 12:31:33 +0700

    lanjut tyling login

--- app/src/main/java/com/example/ngebacot/Login.kt
+++ app/src/main/java/com/example/ngebacot/Login.kt
@@ -3,6 +3,8 @@ package com.example.ngebacot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,8 +33,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ngebacot.ui.theme.NgebacotTheme
@@ -60,14 +69,19 @@ class Login : ComponentActivity() {
 @Composable
 fun BaseCard() {
     val abu = Color(0xFFa1a1a1)
-    var text by remember { mutableStateOf("") }
+    val biruBaseCard = Color(0xFF7C92F5)
+    val btnColorLogin = Color(0xFFFF6978)
+    val outlineInputColor = Color(0xFFFFFFFF)
+    var text by remember { mutableStateOf("")
+
+    }
     Column (
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
     ) {
         Card(
             colors = CardDefaults.cardColors(
-                containerColor = abu,
+                containerColor = biruBaseCard,
             ),
             modifier = Modifier
                 .size(width = 340.dp, height = 320.dp)
@@ -82,7 +96,6 @@ fun BaseCard() {
             Text(
                 text = "Ngebacot",
                 modifier = Modifier
-//                    .padding(6.dp)
                     .align(alignment = Alignment.CenterHorizontally)
             )
             OutlinedTextField(
@@ -90,23 +103,25 @@ fun BaseCard() {
                 onValueChange = { text = it },
                 label = { Text("Username") },
                 modifier = Modifier
-                    .height(60.dp)
+                    .height(75.dp)
                     .width(270.dp)
                     .align(alignment = Alignment.CenterHorizontally)
+                    .padding(bottom = 15.dp)
                 ,
-                shape = RoundedCornerShape(50.dp)
+                shape = RoundedCornerShape(50.dp),
             )
             OutlinedTextField(
                 value = text,
                 onValueChange = { text = it },
                 label = { Text("Password") },
                 modifier = Modifier
-                    .height(60.dp)
+                    .height(75.dp)
                     .width(270.dp)
                     .align(alignment = Alignment.CenterHorizontally)
-//                    .padding(0.dp,10.dp,0.dp,0.dp)
+                    .padding(bottom = 15.dp)
                 ,
-                shape = RoundedCornerShape(50.dp)
+                shape = RoundedCornerShape(50.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(outlineInputColor)
             )
             Button(onClick = { onClick() },
                 modifier = Modifier
@@ -115,15 +130,17 @@ fun BaseCard() {
                     .align(alignment = Alignment.CenterHorizontally)
 //                    .padding(0.dp,10.dp,0.dp,0.dp)
                   ,
-                shape = RoundedCornerShape(50.dp)) {
+                shape = RoundedCornerShape(50.dp),
+                colors = ButtonDefaults.buttonColors(btnColorLogin)
+            ) {
                 Text("Login")
             }
-//            Text(
-//                text = "Ngebacot",
-//                modifier = Modifier
+            Text(
+                text = "Don’t have an account? Sign Up!",
+                modifier = Modifier
 //                    .padding(6.dp)
 //                    .align(alignment = Alignment.CenterHorizontally)
-//            )
+            )
         }
     }
 
