777ac3d5c2442525a2d945321fa49723cfefb19f
lexynotfound <raihanardila@gmail.com>
2023-12-03 13:04:06 +0700

    adding folder to manage data from api

--- app/src/main/java/com/example/ngebacot/ui/theme/Color.kt
+++ app/src/main/java/com/example/ngebacot/ui/theme/Color.kt
@@ -7,6 +7,9 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val abu = Color(0xFFa1a1a1)
+val biruBaseCard = Color(0xFF7C92F5)
+val btnColorLogin = Color(0xFFFF6978)
+val outlineInputColor = Color(0xFFFFFFFF)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
