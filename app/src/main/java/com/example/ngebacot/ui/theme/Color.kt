f172a635e3e654c7e8e3c9b5dcf22842f84b4e62
oscode04 <hellogaso@gmail.com>
2023-12-23 01:12:07 +0700

    bikin halaman profile

--- app/src/main/java/com/example/ngebacot/ui/theme/Color.kt
+++ app/src/main/java/com/example/ngebacot/ui/theme/Color.kt
@@ -9,6 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val abu = Color(0xFFa1a1a1)
 val biruBaseCard = Color(0xFF7C92F5)
 val btnColorLogin = Color(0xFFFF6978)
+val hitamgaitam = Color(0xFF111111)
 val outlineInputColor = Color(0xFFFFFFFF)
 
 val Purple40 = Color(0xFF6650a4)
