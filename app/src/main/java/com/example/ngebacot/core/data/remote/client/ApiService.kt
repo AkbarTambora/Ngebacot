248ae2ae67f6557dcee6c482665944c82a61d239
lexynotfound <raihanardila@gmail.com>
2024-01-02 18:26:41 +0700

    adding interface ApiService

--- app/src/main/java/com/example/ngebacot/core/data/remote/client/ApiService.kt
+++ app/src/main/java/com/example/ngebacot/core/data/remote/client/ApiService.kt
@@ -1,4 +1,20 @@
 package com.example.ngebacot.core.data.remote.client
 
-class ApiService {
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+    //End Poiint Register
+    @POST("register")
+    suspend fun register(@Body request: RequestBody): ResponseBody
+
+    //Endpoint Login
+    @POST("login")
+    suspend fun login(@Body request: RequestBody): ResponseBody
+
+    @POST("logout")
+    suspend fun logout(): ResponseBody
 }
