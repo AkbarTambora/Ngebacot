f26271fef9c486594166c5a6f67eddc8151d85f4
AkbarTambora <77846899+AkbarTambora@users.noreply.github.com>
2024-01-15 01:00:55 +0700

    masih fixing register

--- app/src/main/java/com/example/ngebacot/core/utils/AppConstants.kt
+++ app/src/main/java/com/example/ngebacot/core/utils/AppConstants.kt
@@ -3,5 +3,6 @@ package com.example.ngebacot.core.utils
 class AppConstants {
     companion object{
         const val BASE_URL = "http://ec2-54-211-221-191.compute-1.amazonaws.com/"
+        //const val BASE_URL = "http://54.211.221.191:8800/"
     }
 }
--- app/src/main/java/com/example/ngebacot/views/auth/Login.kt
+++ app/src/main/java/com/example/ngebacot/views/auth/Login.kt
@@ -233,4 +233,76 @@ fun onClick(text: String, text2: String) {
         }
     }*/
 
+    //End Point Register
+//    export const register = (req, res) => {
+//        /*
+//            Check error data
+//         */
+//        // Check if required fields exist in the request body
+//        const { username, email, password } = req.body;
+//
+//        const errors = {};
+//
+//        if (!username) {
+//            errors.username = ["Username must not be blank"];
+//        } else if (username.length < 6) {
+//            errors.username = ["Username must be at least 6 characters long"];
+//        } else if (username.includes(' ')){
+//            errors.username = ["Username cannot contain spaces"];
+//        }
+//
+//        if (!email) {
+//            errors.email = ["Email must not be blank"];
+//        }
+//
+//        if (!password) {
+//            errors.password = ["Password must not be blank"];
+//        } else if (password.length < 8) {
+//            errors.password = ["Password must be at least 8 characters long"];
+//        }
+//
+//        // Check if there are any errors in the request
+//        if (Object.keys(errors).length > 0) {
+//            return res.status(400).json({ errors });
+//        }
+//        /*
+//            Check users if exists
+//         */
+//        // Check if the user already exists
+//        const q = "SELECT * FROM users WHERE username = ? OR email = ?";
+//        db.query(q, [username, email], (err, data) => {
+//        if (err) return res.status(500).json({ message: "Server Error" });
+//        if (data.length) {
+//            const existingUser = data.find(
+//                    user => user.username === username || user.email === email
+//            );
+//            if (existingUser.username === username) {
+//                return res.status(409).json({ message: "Username already exists!" });
+//            } else {
+//                return res.status(409).json({ message: "Email already exists!" });
+//            }
+//        }
+//
+//        // Check email format
+//        const emailFormat = /\S+@\S+\.\S+/;
+//        const isValidEmail = emailFormat.test(email);
+//        if (!isValidEmail) {
+//            return res.status(400).json({ message: "Invalid email format!" });
+//        }
+//
+//        // Hash the password and create a new user
+//        const salt = bcrypt.genSaltSync(10);
+//        const hashedPassword = bcrypt.hashSync(password, salt);
+//
+//        const insertQuery =
+//        "INSERT INTO users (username,email,password,name) VALUE (?)";
+//        const values = [username, email, hashedPassword, req.body.name || null];
+//
+//        db.query(insertQuery, [values], (err, data) => {
+//        if (err) return res.status(500).json({ message: "Failed to create user." });
+//        return res.status(200).json({ message: "User has been created." });
+//        });
+//        });
+//    };
+
 }
