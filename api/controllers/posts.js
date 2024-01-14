4239534e54beb2afeb6cd6ef6873709e9fb79ecb
lexynotfound <raihanardila@gmail.com>
2024-01-14 18:12:54 +0700

    fiexd api

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -4,8 +4,13 @@ import moment from "moment";
 
 export const getPosts = (req, res) => {
     const userId = req.query.userId;
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not logged in!");
+    const authorizationHeader = req.headers['authorization'];
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+        return res.status(401).json("Not logged in!");
+    }
+
+    const token = authorizationHeader.split(' ')[1];
 
     jwt.verify(token, "secretkey", (err, userInfo) => {
         if (err) return res.status(403).json("Token is not valid!");
@@ -14,7 +19,7 @@ export const getPosts = (req, res) => {
 
         const q =
             userId !== "undefined"
-                ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+                ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.created_at DESC`
                 : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
     ORDER BY p.createdAt DESC`;
@@ -28,6 +33,7 @@ export const getPosts = (req, res) => {
         });
     });
 };
+
 
 export const addPost = (req, res) => {
     const authorizationHeader = req.headers['authorization'];
