884d26fdbb6faebb089137eec8ced9df7e3c7c31
lexynotfound <raihanardila@gmail.com>
2024-01-23 12:43:24 +0700

    localdata created

--- app/src/main/java/com/example/ngebacot/screens/HomePage.kt
+++ app/src/main/java/com/example/ngebacot/screens/HomePage.kt
@@ -2,11 +2,10 @@ package com.example.ngebacot.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import com.example.ngebacot.testingjangandipake.iconTwitter
 
 @Composable
 fun HomePage() {
-    iconTwitter()
+    iconTwitters()
     Column {
         BacotanUser(
             msg = Message("Jono", "@kur","Hey, take a look at Jetpack Compose, it's great!")
